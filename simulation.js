57e360b4a99fc9edd9b947fcf6c64139d32f0c2d
John Wigg <john.wigg@gmx.net>
2020-03-20 23:14:33 +0100

    Added simulation parameters

--- simulation.js
+++ simulation.js
@@ -1,1 +1,16 @@
+class Simulation {
+    constructor() {
+        /* Simulation parameters */
+        this.infectivity_distance = 1.0 // Distance at which infection probability reaches 0.5
+        this.minimum_distance = 1.0 // Distance which people keep from each other at minimum
 
+        this.mortality = 0.02 // Mortality rate of the virus
+        this.incubation_period = 14 // Incubation period (days)
+        this.infection_duration = 10 // Duration of the infection until deatch or recovery (days)
+
+        this.infectiveness = 0.9 // Probability of getting infected when someone already carries the virus (e.g. on his hands, should be affected by personal hygiene)
+
+        this.population = 500 // Number of people to simulate
+        this.days_per_sec = 1.0 // Days per second in the simulation
+    }
+}
