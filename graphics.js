94e690d9951f8343a6711e9102cb30e63359bf73
Gorb98 <gorb98@gmail.com>
2020-03-21 14:30:28 +0100

    added colors for simulation

--- graphics.js
+++ graphics.js
@@ -1,5 +1,9 @@
 "use strict";
 (function() { 
+
+    const COLOR_HEALTHY = 0x3E8EFF;
+    const COLOR_INFECTED = 0xE71E3A;
+    const COLOR_RECOVERED = 0x00F214;
     
     class SimulationView extends HTMLElement {
         constructor(persons) {
@@ -17,14 +21,13 @@
                 const graphics = new PIXI.Graphics();
 
                 graphics.lineStyle(0);
-                graphics.beginFill(0xFF3F7D, 1);
+                graphics.beginFill(COLOR_HEALTHY, 1);
                 graphics.drawCircle(0, 0, 6, 6);
                 graphics.endFill();
                 container.addChild(graphics);
 
                 this.containers.push(container)
-                this.app.stage.addChild(container);
-                
+                this.app.stage.addChild(container);                
             }
 
             this.app.ticker.add(() => {this.onTickerUpdate()});
@@ -36,6 +39,33 @@
             for (var i = 0; i < persons.length; i++) {
                 this.containers[i].x = persons[i].position.x * 100;
                 this.containers[i].y = persons[i].position.y * 100;
+
+                //console.log("State: " + persons[i].state + " " + i);
+                if (persons[i].state != persons[i].old_state) {
+                    switch(persons[i].state) {
+                        case "infected":
+                            this.updateCircle(this.containers[i], COLOR_INFECTED);
+                            break;
+                        case "recovered":
+                            this.updateCircle(this.containers[i], COLOR_RECOVERED);
+                            break;
+                        default:
+                            this.updateCircle(this.containers[i], COLOR_HEALTHY);
+                            break;
+                    }
+
+                }
+            }
+        }
+
+        updateCircle(container, color) {
+            console.log("updateCircle()");
+            for (var i = 0; i < container.children.length; i++) {
+                container.children[i].clear();
+                container.children[i].lineStyle(0);
+                container.children[i].beginFill(color, 1);
+                container.children[i].drawCircle(0, 0, 6, 6);
+                container.children[i].endFill();
             }
         }
     }
