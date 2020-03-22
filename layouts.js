3a6b26dd1b968979e2bab717ee587fdaf7044c5e
Niklas <n.koinzer@web.de>
2020-03-22 14:15:21 +0100

    layout-info

--- layouts.js
+++ layouts.js
@@ -49,9 +49,11 @@ function showLayout(){
 
 }
 
-function showLayout-info(){
+function showLayoutInfo(){
     var body = document.body;
     body.innerHTML="";
+    
+    //Header
     var header = document.createElement("header"); 
     body.appendChild(header);
     var elem = document.createElement("img");
@@ -62,7 +64,46 @@ function showLayout-info(){
     header.appendChild(elem);
     elem.setAttribute("class","topOption");
     elem.setAttribute("src","assets/share-24px.svg");
-    
+    var elem = document.createElement("img");
+    body.appendChild(elem);
+    elem.setAttribute("class","small-virus");
+    elem.setAttribute("src","assets/virus.svg");
+
+    //Main
+    var main = document.createElement("main");
+    body.appendChild(main);
+    var elem = document.createElement("h4");
+    main.appendChild(elem);
+    elem.setAttribute("Style","text-align: center; text-transform: uppercase;");
+    elem.innerHTML = "What is the corona-virus?";
+    var elem = document.createElement("p");
+    main.appendChild(elem);
+    elem.innerHTML = "Test";
+    var elem = document.createElement("a");
+    main.appendChild(elem);
+    elem.setAttribute("class","more-info");
+    elem.setAttribute("href","");
+    elem.innerHTML = "More information...";
+
+    //Footer
+    var footer = document.createElement("footer");
+    body.appendChild(footer);
+    footer.setAttribute("class","footer-controls");
+    var div = document.createElement("div");
+    footer.appendChild(div);
+    div.setAttribute("class","arrow-container");
+    var elem = document.createElement("img");
+    div.appendChild(elem);
+    elem.setAttribute("class","arrow-left");
+    elem.setAttribute("src","assets/arrow_left.svg");
+    var elem = document.createElement("span");
+    div.appendChild(elem);
+    elem.setAttribute("class","disable-margin");
+    elem.innerHTML = "1/6";
+    var elem = document.createElement("img");
+    div.appendChild(elem);
+    elem.setAttribute("class","arrow-right");
+    elem.setAttribute("src","assets/arrow_right.svg");
 }
 
-window.addEventListener("load", showLayout);
+window.addEventListener("load", showLayoutInfo);
